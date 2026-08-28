bdf6de2dd4c29a5ab2a2c0e07fd8a1fcd24f2e77
6Morpheus6 <wanton-decay@gmx.net>
2026-08-28 04:06:51 +0200

    update

--- torch.js
+++ torch.js
@@ -29,9 +29,13 @@ module.exports = {
       "when": "{{platform === 'linux' && gpu === 'amd'}}",
       "method": "shell.run",
       "params": {
+        "bluefairy": "off",
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/rocm6.3 --force-reinstall --no-deps"
+        "message": [
+          "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/rocm6.3 --force-reinstall",
+          "uv pip install numpy==1.26.0"
+        ]
       }
     }
   ]
